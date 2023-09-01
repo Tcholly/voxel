747e4340cd55440d0c0a1b9a270371eb66abe71c
Tcholly <roberti.gionata@protonmail.ch>
2023-09-02 01:55:11 +0200

    Fixed build.sh script

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -xe
 
 gcc -Wall -Wextra -ggdb -o voxel src/main.c src/world.c src/chunk.c src/utils/math.c src/utils/mesh.c -Isrc -IDependencies/raylib/include -LDependencies/raylib/lib -lm -lraylib
 
-if [ $1="run" ]; then
+if [ "$1" = "run" ]; then
 	./voxel
 fi
